04d768c3055d78bf927841460a6e29886b6f0607
1TAEKIM <oneteak1@gmail.com>
2024-04-12 14:17:25 +0900

    practice_6_ing

--- moives_blog/moives_blog/settings.py
+++ moives_blog/moives_blog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "movies",
     "accounts",
+    "reviews",
     "django_extensions",
     "django.contrib.admin",
     "django.contrib.auth",
--- moives_blog/moives_blog/urls.py
+++ moives_blog/moives_blog/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('reviews/', include('reviews.urls'))
 ]
