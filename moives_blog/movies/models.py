94ab302f5f91fd33d1a8de2281e78a6879928ee6
1TAEKIM <oneteak1@gmail.com>
2024-04-12 12:09:12 +0900

    practice_5 완료

--- moives_blog/movies/models.py
+++ moives_blog/movies/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Movie(models.Model):
     # 각 변수값은 필드명
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
     content = models.TextField()
+    is_open = models.BooleanField()
+    
+    created_at = models.DateField(auto_now_add=True)
+    
+    
+    
--- moives_blog/movies/views.py
+++ moives_blog/movies/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Movie
 
-def main_page(request):
-    print(request.GET)
+def main(request):
 
-    req_data = request.GET
-
+    movies=  Movie.objects.all()
     context = {
-       'create_movie' : req_data.get('create_movie') 
-        if req_data else '영화가 없습니다.'
+        
+        'movies': movies
     }
 
     return render(request, 'movies/main_page.html', context)
@@ -15,11 +14,52 @@ def main_page(request):
 
 
 def create_movie(request):
+    
+    if request.method == 'POST':
+        data = request.POST
+        print(data)
+        
+        new_movie = Movie.objects.create(
+            title=data.get('create_movie_title'),
+            content=data.get('create_movie_content')
+        )
+        
+        return redirect('movies:detail', pk=new_movie.pk)
 
     return render(request, 'movies/create_movie.html')
 
 
-def detail_page(request):
+def detail(request, pk):
 
+    movie = Movie.objects.get(pk=pk)
+    context = {
+        'movie' : movie
+    }
+    
+    if request.method == 'POST':
+        data = request.POST
+        print(data)
+        
+        Movie.objects.filter(pk=pk).delete()
+        
+        return redirect('movies:main')
+    
+    return render(request, 'movies/detail_page.html', context)
 
-    return render(request, 'movies/detail_page.html')
+def delete(request, pk):
+    
+    data = request.POST
+    Movie.objects.filter(pk=pk).delete()
+        
+    return redirect('movies:main')
+
+def update(request, pk):
+    
+    if request.method == 'POST':
+        data = request.POST
+        Movie.objects.filter(pk=pk).update(
+            title=data.get('update_movie_title'), content=data.get('update_movie_content'))
+        
+        return redirect('movies:detail', pk=pk)
+
+    return render(request, 'movies/update_page.html')
